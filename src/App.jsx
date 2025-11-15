ebd630cbefcaac06f5f8007d1fea7d245be15cd4
Suchit-inside <inside.suchit@gmail.com>
2025-11-15 17:47:37 +0530

    5th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
 
 const App = () => {
 
--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,16 +1,17 @@
 import useMacbookStore from "../store";
 import clsx from "clsx";
 import {Canvas} from "@react-three/fiber";
+import {Box, OrbitControls} from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14.jsx";
 
-// import StudioLights from "./three/StudioLights.jsx";
-// import ModelSwitcher from './three/ModelSwitcher.jsx'
+import StudioLights from "./three/StudioLights.jsx";
+import ModelSwitcher from './three/ModelSwitcher.jsx'
 import {useMediaQuery} from "react-responsive";
-import {MacbookModel14} from "./models/Macbook-14.jsx";
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore();
 
-    // const isMobile = useMediaQuery({ query: '(max-width: 1024px)'});
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px)'});
 
     return (
         <section id="product-viewer">
@@ -49,8 +50,10 @@ const ProductViewer = () => {
             </div>
 
             <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
-              <MacbookModel14 scale={0.06} position={[-1, 0, 0]}/>
-               </Canvas>
+                <StudioLights />
+
+                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
+            </Canvas>
         </section>
     )
 }
